2f600bf0178c698481698e8537456dc66c3729f4
simon <smunier@excilys.com>
2016-06-16 22:14:48 +0200

    [examples][clean-up] move some dependencies to npm

--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.use('/shaders/', express.static(__dirname + '/shaders/'));
 app.use('/libs/', express.static(__dirname + '/libs/'));
+app.use('/npmlibs/', express.static(__dirname + '/node_modules/'));
 app.use('/', express.static(__dirname + '/src/'));
 
 app.listen(3000);
